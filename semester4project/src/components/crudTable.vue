6aac5a0af943ce9bc3fe3066df6c8a02a38637b8
paripasd <paripasd@gmail.com>
2023-04-06 13:14:03 +0200

    ui table selected feature

--- semester4project/src/components/crudTable.vue
+++ semester4project/src/components/crudTable.vue
@@ -2,17 +2,17 @@
     <div class="px-4 sm:px-6 lg:px-8 flow-root">
         <div class="-my-2 -mx-4 sm:-mx-6 lg:-mx-8">
           <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-            <table class="min-w-full border-separate highlight-selected-row overflow-hidden">
+            <table class="min-w-full overflow-hidden">
               <thead>
                 <tr class="divide-x sticky">
                   <th class="sticky overflow-y-hidden top-0 z-10 py-3.5 pl-4 pr-4 text-left rounded-md text-sm font-semibold text-gray-900 bg-gray-100">Question</th>
                   <th class="sticky top-0 z-10 py-3.5 pl-4 pr-4 text-left text-sm rounded-md font-semibold text-gray-900 bg-gray-100">Answer</th>
                 </tr>
               </thead>
-                <tbody class="divide-y divide-gray-200 bg-white">
-                  <tr v-on:click= " getRow(q)" v-for="q in qna" :key="q.id" class="divide-x hover:bg-gray-200">
-                    <td class="rounded-lg whitespace-normal w-1/3 max-w-[80px] py-4 pl-4 pr-4 text-sm font-medium text-gray-900 sm:pl-0">{{ q.question }}</td>
-                    <td class="rounded-lg whitespace-normal w-2/3 max-w-screen-sm py-4 pl-4 pr-4 text-sm text-gray-500 sm:pr-0">{{ q.answer }}</td>
+                <tbody class="bg-white">
+                  <tr v-on:click= " getRow(q,$event)" v-for="q in qna" :key="q.id" class="divide-x hover:bg-gray-200">
+                    <td class="whitespace-normal w-1/3 max-w-[80px] py-4 pl-4 pr-4 text-sm font-medium text-gray-900 sm:pl-0">{{ q.question }}</td>
+                    <td class="whitespace-normal w-2/3 max-w-screen-sm py-4 pl-4 pr-4 text-sm text-gray-500 sm:pr-0">{{ q.answer }}</td>
                   </tr>
                 </tbody>
             </table>
@@ -37,14 +37,23 @@
     });
   });
 
-  function getRow(q) {
+  let lastClickedRow = null;
+
+  function getRow(q, event) {
     const unit = {question:q.question, answer:q.answer, comment:q.comment, id:q.id, path:q.path}
     store.updateUnit(unit);
-  }
 
- 
-  
-  </script>
+    const rows = document.querySelectorAll('tr');
+    rows.forEach((r) => {
+        r.classList.remove('border-2','border-blue-500','rounded');
+    });
+
+    const currentRow = event.target.parentNode;
+    currentRow.classList.add('border-2','border-blue-500', 'rounded');
+
+    lastClickedRow = currentRow;
+  }
+</script>
 
 
 
